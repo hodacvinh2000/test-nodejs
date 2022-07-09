ff7db868f98465c0c82b205e76eba60413ad9671
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-07-09 18:43:11 +0700

    console log req.url

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((req, res, next) => {
+  console.log(req.url);
+});
 
 server.listen(process.env.PORT || 3000);
