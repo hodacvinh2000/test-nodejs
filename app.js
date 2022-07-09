91453e478b4f7df25b8494a60abd51b9a8b28032
Ho_Dac_Vinh <hodacvinh2000@gmail.com>
2022-07-09 18:45:26 +0700

    show hello world

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require("http");
 
 const server = http.createServer((req, res, next) => {
-  console.log(req.url);
+  res.write("hello world!");
+  res.end();
 });
 
 server.listen(process.env.PORT || 3000);
